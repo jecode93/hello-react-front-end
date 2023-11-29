1043d53169ebdc6393a34b400f0bef327a1b54d2
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-29 12:25:08 -0500

    Complete front end part.

---
+++ src/redux/greeting/greetingSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const URL = '/api/v1/greetings';
+
+export const getRandomGreeting = createAsyncThunk(
+  'greeting/getRandomGreeting',
+  async () => {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
+);
+
+const initialState = {
+  greeting: '',
+  isLoading: false,
+  error: undefined,
+};
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getRandomGreeting.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getRandomGreeting.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.greeting = action.payload.message;
+      })
+      .addCase(getRandomGreeting.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
